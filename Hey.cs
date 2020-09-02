7badda4a3e3a4a11c0b4107d578a4beb7c376a10
LiterallyFabian <lindrenfabian@gmail.com>
2020-09-02 10:58:49 +0200

    Add script

---
+++ Hey.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Net;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string username = "";
+        string password = "";
+        string URI = "https://sms.schoolsoft.se/pps/jsp/Login.jsp";
+        string myParameters = $"action=login&usertype=1&ssusername={username}&sspassword={password}&button=Logga+in";
+
+        using (WebClient wc = new WebClient())
+        {
+            int i = 0;
+            while (true)
+            {
+                wc.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+                wc.UploadString(URI, myParameters);
+                Console.WriteLine(i++);
+            }
+        }
+    }
+}

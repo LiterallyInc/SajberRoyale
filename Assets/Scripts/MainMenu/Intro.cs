32981efed38341088a54fe3c67a734c8f195b41f
Fabian <lindgrenfabian@gmail.com>
2020-09-23 10:57:54 +0200

    Add intro sequence to game

---
+++ Assets/Scripts/MainMenu/Intro.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SajberRoyale.MainMenu
+{
+    public class Intro : MonoBehaviour
+    {
+        public float introLenght;
+        private void Start()
+        {
+            StartCoroutine(Wait());
+        }
+
+        private IEnumerator Wait()
+        {
+            yield return new WaitForSeconds(introLenght);
+            SceneManager.LoadScene(1);
+        }
+    }
+}

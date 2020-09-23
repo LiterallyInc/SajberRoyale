7f76725cdae683a5c283ec10070f168fe283941b
Fabian <lindgrenfabian@gmail.com>
2020-09-23 09:47:13 +0200

    Update admin panel

--- Assets/Scripts/Netcode/NetworkAdmin.cs
+++ Assets/Scripts/Netcode/NetworkAdmin.cs
@@ -1,45 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Photon.Pun;
 using UnityEngine;
-using Photon.Pun;
 using UnityEngine.UI;
-public class NetworkAdmin : MonoBehaviour
+
+namespace SajberRoyale.UI
 {
-    public Canvas AdminCanvas;
-    public bool AdminMenuOpen = false;
-    public InputField IF_Password;
-    public InputField IF_Roomcode;
+    public class NetworkAdmin : MonoBehaviour
+    {
+        public Canvas AdminCanvas;
+        public bool AdminMenuOpen = false;
+        public InputField IF_Password;
+        public InputField IF_Roomcode;
 
-    readonly string hash = "A169577AB6A460D28C9BBF3ACDD5C653E153BBA872106E79B49EBC251C615CD0C7C0D3213A84A0FD2EBA945FAC161A3743BDFE4DF9297EBC392504CB6C6A0EAE";
-    private void Start()
-    {
-        Object.DontDestroyOnLoad(AdminCanvas.gameObject);
-    }
-    private void Update()
-    {
-        //toggle menu
-        if (Input.GetKeyDown(KeyCode.F1))
+        private const string passwordhash = "A169577AB6A460D28C9BBF3ACDD5C653E153BBA872106E79B49EBC251C615CD0C7C0D3213A84A0FD2EBA945FAC161A3743BDFE4DF9297EBC392504CB6C6A0EAE";
+
+        private void Start()
         {
-            transform.localScale = AdminMenuOpen ? Vector3.zero : Vector3.one;
-            AdminMenuOpen = !AdminMenuOpen;
+            Object.DontDestroyOnLoad(AdminCanvas.gameObject);
         }
-        if (IF_Password.text == "" && !Application.isEditor) return;
-        string hashedpass = IF_Password.text.SHA512();
 
-        //allow all fields if logged in or in editor
-        bool correct = (hashedpass == hash || Application.isEditor) && PhotonNetwork.IsConnected;
-        IF_Roomcode.interactable = correct;
+        private void Update()
+        {
+            //toggle menu
+            if (Input.GetKeyDown(KeyCode.F1))
+            {
+                transform.localScale = AdminMenuOpen ? Vector3.zero : Vector3.one;
+                AdminMenuOpen = !AdminMenuOpen;
+            }
+            TryToLogin();
+        }
+        private void TryToLogin()
+        {
+            bool loggedin = Application.isEditor || IF_Password.text.SHA512() == passwordhash;
+            IF_Password.interactable = !loggedin;
+            IF_Roomcode.interactable = loggedin && PhotonNetwork.IsConnected;
+        }
 
-        //no need to edit pass once correct 
-        if (Application.isEditor) 
-            IF_Password.interactable = false;
-        else
-            IF_Password.interactable = hashedpass != hash;
-    }
-    //called by UI
-    public void CreateRoom(string name)
-    {
-        PhotonNetwork.CreateRoom(name);
-        Webhook.Send($"**{PhotonNetwork.NickName}** created a room called \"**{name}**\".");
+        #region Called by UI
+
+        public bool CreateRoom(string name)
+        {
+            Webhook.Send($"**{PhotonNetwork.NickName}** created a room called \"**{name}**\".");
+            return PhotonNetwork.CreateRoom(name);
+        }
+
+        #endregion Called by UI
     }
 }

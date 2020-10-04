127e661e55b8992a000ca1e2671efcf33a71e96c
Fabian <lindgrenfabian@gmail.com>
2020-10-04 13:57:05 +0200

    Add music and fades to main

---
+++ Assets/Scripts/MainMenu/DeleteAfter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DeleteAfter : MonoBehaviour
+{
+    public float Delay;
+
+    private void Start()
+    {
+        StartCoroutine(DeleteIn());
+    }
+
+    private IEnumerator DeleteIn()
+    {
+        yield return new WaitForSeconds(Delay);
+        Destroy(this.gameObject);
+    }
+}

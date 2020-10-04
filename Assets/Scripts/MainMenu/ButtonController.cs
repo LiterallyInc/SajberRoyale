18e3b73e375a780ee7265801076837445e872b81
Fabian <lindgrenfabian@gmail.com>
2020-10-04 13:07:43 +0200

    Create test cinematic for main

---
+++ Assets/Scripts/MainMenu/ButtonController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    public void ConnectToServer()
+    {
+
+    }
+
+    public void JoinRoom()
+    {
+
+    }
+}

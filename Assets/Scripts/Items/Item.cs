d38ba2401c8fa1f86ae33bbf2d11c2658ac0cb03
Fabian <lindgrenfabian@gmail.com>
2020-10-06 21:55:51 +0200

    Create test classes for items

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    #region Local variables
+
+    /// <summary>
+    /// Localized & stylized name of the item showing up in-game.
+    /// Example: Diamond Sword
+    /// </summary>
+    public string name;
+
+    /// <summary>
+    /// Description of the item showing up when hovered in-game.
+    /// </summary>
+    public string description;
+
+    /// <summary>
+    /// Key for accessing item through dictionary.
+    /// Example: diamondsword
+    /// </summary>
+    public string ID;
+
+    /// <summary>
+    /// Icon for item loaded into the hotbar.
+    /// </summary>
+    public Sprite icon;
+
+    /// <summary>
+    /// Prefab for item spawned in-game.
+    /// </summary>
+    public GameObject item;
+
+    /// <summary>
+    /// Max amount of this item that can be held at the same time.
+    /// </summary>
+    public int maxStack;
+
+    /// <summary>
+    /// Category of item.
+    /// </summary>
+    public Type type;
+
+    #endregion
+
+    public enum Type
+    {
+        Weapon,
+        Ammo,
+        Healing
+    }
+}
---
+++ Assets/Scripts/Items/Weapon.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : Item
+{
+    /// <summary>
+    /// Damage output from weapon is randomized between these two values
+    /// </summary>
+    public float minDamage, maxDamage;
+
+    /// <summary>
+    /// Delay between shots
+    /// </summary>
+    public float shootingDelay;
+
+    /// <summary>
+    /// Wether the trigger can be held or not
+    /// </summary>
+    public bool isAuto;
+
+    /// <summary>
+    /// Wether the weapon is on cooldown or not
+    /// </summary>
+    public bool hasShot;
+
+    /// <summary>
+    /// Size of the weapon clip
+    /// </summary>
+    public int clipSize;
+
+    /// <summary>
+    /// Played every time the weapon is shot
+    /// </summary>
+    public AudioClip shootSFX;
+
+
+}

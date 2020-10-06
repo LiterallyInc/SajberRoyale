0219dd73e647f9200300f6d4a56c86e6cfbcb8f6
Fabian <lindgrenfabian@gmail.com>
2020-10-06 21:09:43 +0200

    Add option to skip intro

--- Assets/Scripts/MainMenu/Intro.cs
+++ Assets/Scripts/MainMenu/Intro.cs
@@ -7,9 +7,15 @@ namespace SajberRoyale.MainMenu
     public class Intro : MonoBehaviour
     {
         public float introLenght;
+
         private void Start()
         {
             StartCoroutine(Wait());
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) SceneManager.LoadScene(1);
         }
 
         private IEnumerator Wait()

158552288da42f270187b0825fde0912c121f818
Fabian <lindgrenfabian@gmail.com>
2020-12-02 10:15:38 +0100

    add extension methods for byte/object converting

---
+++ Assets/Scripts/Extensions/ByteConverting.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+internal static class ByteConverting
+{
+    public static byte[] ToByteArray(this object obj)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        using (var ms = new MemoryStream())
+        {
+            bf.Serialize(ms, obj);
+            return ms.ToArray();
+        }
+    }
+
+    public static object ToObject(this byte[] arrBytes)
+    {
+        using (var memStream = new MemoryStream())
+        {
+            var binForm = new BinaryFormatter();
+            memStream.Write(arrBytes, 0, arrBytes.Length);
+            memStream.Seek(0, SeekOrigin.Begin);
+            var obj = binForm.Deserialize(memStream);
+            return obj;
+        }
+    }
+}

612efa48c0580cea199200024f881c39b281f854
Fabian <lindgrenfabian@gmail.com>
2021-01-22 11:56:40 +0100

    Add speed modifier to clock

--- Assets/Scripts/Game/Clock.cs
+++ Assets/Scripts/Game/Clock.cs
@@ -3,22 +3,24 @@ using UnityEngine;
 
 public class Clock : MonoBehaviour
 {
-    public GameObject Sec;
-    public GameObject Min;
-    public GameObject Hour;
+    public Transform Sec;
+    public Transform Min;
+    public Transform Hour;
+    public int modifier;
 
     private void Start()
     {
-        Sec.transform.rotation = Quaternion.Euler(new Vector3(-90 - DateTime.Now.Second * 6, 0, 0));
-        Min.transform.rotation = Quaternion.Euler(new Vector3(-90 - DateTime.Now.Minute * 6, 0, 0));
-        Hour.transform.rotation = Quaternion.Euler(new Vector3(-90 - DateTime.Now.Hour * 30, 0, 0));
-        InvokeRepeating("UpdateClock", 1, 0.1666f);
+        Sec.rotation = Quaternion.Euler(new Vector3(-90 - DateTime.Now.Second * 6, 0, 0));
+        Min.rotation = Quaternion.Euler(new Vector3(-90 - DateTime.Now.Minute * 6, 0, 0));
+        Hour.rotation = Quaternion.Euler(new Vector3(-90 - DateTime.Now.Hour * 30, 0, 0));
+        InvokeRepeating("UpdateClock", 1, 0.16666f);
     }
 
     private void UpdateClock()
     {
-        Sec.transform.Rotate(new Vector3(-1, 0, 0));
-        Min.transform.Rotate(new Vector3(-1 / 60, 0, 0));
-        Hour.transform.Rotate(new Vector3(-1 / 60 / 90, 0, 0));
+        Sec.Rotate(new Vector3(-1 * modifier, 0, 0));
+        Min.Rotate(new Vector3(-1 / 60 * modifier, 0, 0));
+        Hour.Rotate(new Vector3(-1 / 60 / 90 * modifier, 0, 0));
+        Debug.Log($"mo{modifier}");
     }
 }

d706afb87dc574e4e387ceb91ba33bf25e8830aa
Fabian <lindgrenfabian@gmail.com>
2021-01-22 23:40:24 +0100

    add cinematic template

---
+++ Assets/Scripts/MainMenu/LobbyCinematic.cs
@@ -0,0 +1,14 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyCinematic : MonoBehaviour
+{
+    public float duration = 1;
+    public CinemachineVirtualCamera cam;
+    public void Activate()
+    {
+
+    }
+}

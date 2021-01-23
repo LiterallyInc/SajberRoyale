2c3d448c6c23e1343a50379ad246f65e08e2e3ad
Fabian <lindgrenfabian@gmail.com>
2021-01-24 00:38:08 +0100

    Sync player animations between clients

--- Assets/Scripts/Netcode/PlayerSync.cs
+++ Assets/Scripts/Netcode/PlayerSync.cs
@@ -6,30 +6,46 @@ public class PlayerSync : MonoBehaviourPun
     public GameObject CameraController;
     public EasySurvivalScripts.PlayerMovement PlayerMovement;
 
-    public GameObject[] Meshes;
+    public string[] Meshes;
+    private GameObject Player;
 
     private void Start()
     {
         if (!PhotonNetwork.IsConnected) return;
+
+        //seed character with owner ID to sync them
         Random.InitState(Core.seed * photonView.OwnerActorNr);
-        GameObject newMesh = Instantiate(Meshes[Random.Range(0, Meshes.Length)], gameObject.transform);
+
+        //delete local comps if it's another user
         if (!photonView.IsMine)
         {
             Destroy(CameraController);
             Destroy(PlayerMovement);
         }
-        else
+        else //else instansiate the avatar and set the animator
         {
-            PlayerMovement.CharacterAnimator = newMesh.GetComponent<Animator>();
+            Player = PhotonNetwork.Instantiate($"CharMeshes/{Meshes[Random.Range(0, Meshes.Length)]}", Vector3.zero, Quaternion.identity);
+            PlayerMovement.CharacterAnimator = Player.GetComponent<Animator>();
         }
+        //place the other avatars
+        foreach(GameObject g in GameObject.FindGameObjectsWithTag("Avatar"))
+        {
+            if(g.GetComponent<PhotonView>().ControllerActorNr == photonView.ControllerActorNr)
+            {
+                g.transform.parent = gameObject.transform;
+                g.transform.localPosition = new Vector3(-0.01f, -1.16f, -0.1f);
+            }
+        }
+        
     }
 
     private void Update()
     {
         if (Application.isEditor && Input.GetKeyDown(KeyCode.K))
         {
-            GameObject newMesh = Instantiate(Meshes[Random.Range(0, Meshes.Length)], gameObject.transform);
-            PlayerMovement.CharacterAnimator = newMesh.GetComponent<Animator>();
+            Player.GetComponent<PhotonView>().ObservedComponents.Add(Player.GetComponent<PhotonAnimatorView>());
+            //GameObject newMesh = Instantiate(Meshes[Random.Range(0, Meshes.Length)], gameObject.transform);
+            //PlayerMovement.CharacterAnimator = newMesh.GetComponent<Animator>();
         }
     }
 }

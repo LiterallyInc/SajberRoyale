5aefba80b627d106796c8feb85f3006035ed7824
Fabian <lindgrenfabian@gmail.com>
2021-01-24 20:29:24 +0100

    Add audio to clock

--- Assets/Scripts/Game/Clock.cs
+++ Assets/Scripts/Game/Clock.cs
@@ -8,18 +8,29 @@ public class Clock : MonoBehaviour
     public Transform Hour;
     public int modifier;
 
+    public AudioClip Tick;
+    public AudioClip Tock;
+    public bool hasTocked = false;
+
     private void Start()
     {
         Sec.rotation = Quaternion.Euler(new Vector3(-90 - DateTime.Now.Second * 6, 0, 0));
         Min.rotation = Quaternion.Euler(new Vector3(-90 - DateTime.Now.Minute * 6, 0, 0));
         Hour.rotation = Quaternion.Euler(new Vector3(-90 - DateTime.Now.Hour * 30 - DateTime.Now.Minute*0.5f, 0, 0));
-        InvokeRepeating("UpdateClock", 1, 0.16666f);
+        InvokeRepeating("UpdateClock", 1, 1);
+        InvokeRepeating("TickTock", 0.7f, 1f);
     }
 
     private void UpdateClock()
     {
-        Sec.Rotate(new Vector3(-1 * modifier, 0, 0));
-        Min.Rotate(new Vector3(-1 * modifier / 60, 0, 0));
-        Hour.Rotate(new Vector3(-1 * modifier / 60 / 90 , 0, 0));
+        Sec.Rotate(new Vector3(-6 * modifier, 0, 0));
+        Min.Rotate(new Vector3(-6 * modifier / 60, 0, 0));
+        Hour.Rotate(new Vector3(-6 * modifier / 60 / 90 , 0, 0));
+    }
+    private void TickTock()
+    {
+        GetComponent<AudioSource>().clip = hasTocked ? Tick : Tock;
+        GetComponent<AudioSource>().Play();
+        hasTocked = !hasTocked;
     }
 }

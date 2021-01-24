cd9fea9950d24a4424cdacc5545cf1b018b4bbe3
Fabian <lindgrenfabian@gmail.com>
2021-01-24 23:46:50 +0100

    Add HUD compass

---
+++ Assets/Scripts/Game/HUD/CompassHandler.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CompassHandler : MonoBehaviour
+{
+    public RawImage Compass;
+
+    private void Update()
+    {
+        if(Camera.main) Compass.uvRect = new Rect((Camera.main.gameObject.transform.parent.localEulerAngles.y-90) / 360, 0, 1, 1);
+    }
+}

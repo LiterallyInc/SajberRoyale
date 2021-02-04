7eef32c3e982b912dd3d4d74ac06ec278b455017
Fabian <lindgrenfabian@gmail.com>
2021-02-04 23:15:26 +0100

    add the beautiful H524 painting

---
+++ Assets/Scripts/Game/PanoramaHandler.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PanoramaHandler : MonoBehaviour
+{
+    [Header("Colliders")]
+    public BoxCollider Haga;
+    public BoxCollider Vanadis;
+
+    [Header("Panoramas")]
+    public GameObject V_W;
+    public GameObject V_S;
+    public GameObject H_N;
+    public GameObject H_S;
+    void Update()
+    {
+        
+    }
+}

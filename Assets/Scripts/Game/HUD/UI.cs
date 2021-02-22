0580b3cdd7ea3bdc43c8adf6731c87e598a963c6
Fabian <lindgrenfabian@gmail.com>
2021-02-22 16:55:55 +0100

    show in hud when player dies

--- Assets/Scripts/Game/HUD/UI.cs
+++ Assets/Scripts/Game/HUD/UI.cs
@@ -33,7 +33,7 @@ namespace SajberRoyale.Game
 
         private void UpdateStats()
         {
-            GameStats.text = $@"{(Helper.IsDev ? "<color=#FFB4C0>[DEV]</color> " : "")}{(Game.Instance.IsAlive ? "" : "<color=#FD6F77>[DEAD]</color>")}{PhotonNetwork.NickName}
+            GameStats.text = $@"{(Helper.IsDev ? "<color=#FFB4C0>[DEV]</color> " : "")}{(Game.Instance.IsAlive ? "" : "<color=#FD6F77>[DEAD]</color> ")}{PhotonNetwork.NickName}
 <size=10><i>Playing as {Game.Instance.Skin}</i></size>
 
 Room: {Game.Instance.CurrentRoom}

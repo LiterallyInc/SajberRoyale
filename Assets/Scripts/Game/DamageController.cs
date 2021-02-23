47741f053b4c17343b0ca469052250717156ae3c
LiterallyFabian <lindrenfabian@gmail.com>
2021-02-23 13:21:01 +0100

    Play shooting sound when player hits

--- Assets/Scripts/Game/DamageController.cs
+++ Assets/Scripts/Game/DamageController.cs
@@ -37,7 +37,9 @@ namespace SajberRoyale.Player
             {
             }
             AnimateWeapon(weaponID, info.Sender.ActorNumber);
+            Weapon weapon = (Weapon)ItemDatabase.Instance.GetItem(weaponID);
             PlayAudioAtPlayer(actorID, 5, damageSounds[Random.Range(0, damageSounds.Length)]);
+            PlayAudioAtPlayer(info.Sender.ActorNumber, weapon.range * 1.25f, weapon.shootSFX[Random.Range(0, weapon.shootSFX.Length)]);
         }
 
         /// <summary>

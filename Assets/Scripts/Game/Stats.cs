e0e51798709152882f9d9f9e06294d19b6fa6a15
Fabian <lindgrenfabian@gmail.com>
2021-03-08 20:55:01 +0100

    add more stats

--- Assets/Scripts/Game/Stats.cs
+++ Assets/Scripts/Game/Stats.cs
@@ -6,9 +6,13 @@ namespace SajberRoyale.Game
     {
         public long StartEpoch;
         public int Eliminations = 0;
+        public int DamageDone = 0;
+        public int DamageTaken = 0;
+        public int HPRegen = 0;
         public int ShotsFired = 0;
         public int ShotsHit = 0;
-        public int HPRegen = 0;
+        public int ItemsPickup = 0;
+        public int LockersOpened = 0;
         public int Emotes = 0;
         public int Placement = 1;
     }

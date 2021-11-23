ff3a337c89392394969be75b4ad86a222941e3be
Fabian Lindgren <lindgrenfabian@gmail.com>
2021-11-23 15:27:56 +0100

    Add teleport areas for VR

--- Assets/Scripts/Game/InvisibleTeleport.cs
+++ Assets/Scripts/Game/InvisibleTeleport.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class InvisibleTeleport : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        GetComponent<MeshRenderer>().enabled = false;
     }
 }
